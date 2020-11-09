6b0e27b44a5dc65eb48845882d96a9a4d6c94d05
Thi <dinhanhthi@gmail.com>
2020-11-10 00:15:19 +0100

    or-tool

---
+++ employee-scheduling-or-tool-trong.py
@@ -0,0 +1,10 @@
+# team có 50 người, chia ngày 3 ca (2 ca ngày, 1 ca đêm), mỗi ca cần 15 người, chia ca làm sao để đảm bảo số ca ngày đêm của mỗi người bằng nhau,
+# với cả khoảng cách giữ 2 ca làm liên tiếp của 1 người phải 16h (bằng thời gian 2 ca làm)
+
+from ortools.sat.python import cp_model
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
